8aee4419d04c3af1034279ff65e93f89ae127cc4
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-18 10:31:33 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,61 @@
+class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except:[:create, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :edit]
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.new
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+    @prototype = Prototype.new
+    @prototype = Prototype.find(params[:id])
+  end
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to  prototypes_path(@prototype)
+  end
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless current_user = @prototype.user
+      redirect_to action: :index
+    end
+  end
+
+end
